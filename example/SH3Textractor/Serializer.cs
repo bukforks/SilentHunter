8a2ed2d4959fd65ab9b054f1da0f3ea4cd067263
Bukowa <gitbukowa@gmail.com>
2024-05-10 06:15:09 +0200

    feat: add sh3xtractor
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

---
+++ example/SH3Textractor/Serializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SH3Textractor;
+
+public static class Serializer
+{
+	public class ShouldSerializeContractResolver : DefaultContractResolver
+	{
+		public static readonly ShouldSerializeContractResolver Instance =
+			new ShouldSerializeContractResolver();
+
+		protected override JsonProperty CreateProperty
+		(
+			MemberInfo member,
+			MemberSerialization memberSerialization)
+		{
+			JsonProperty property = base.CreateProperty(member, memberSerialization);
+			// example of skipping serialization of certain properties
+
+			// if( property.DeclaringType == typeof(ModelChunk) &&
+			//    property.PropertyName == "VertexIndices" ||
+			//    property.PropertyName == "TextureIndices" ||
+			//    property.PropertyName == "Vertices" ||
+			//    property.PropertyName == "TextureCoordinates" ||
+			//    property.PropertyName == "MaterialIndices" ||
+			//    property.PropertyName == "Normals" )
+			//    
+			// {
+			// 	property.ShouldSerialize = instance => false;
+			// }
+
+			return property;
+		}
+	}
+}
